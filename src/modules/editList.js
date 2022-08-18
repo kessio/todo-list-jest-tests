4c5221802561fc0d11849fef53e682589b116a90
kessio <64524758+kessio@users.noreply.github.com>
2022-08-18 17:50:27 +0300

    Arranging functions

---
+++ src/modules/editList.js
@@ -0,0 +1,19 @@
+import editList from './editList.js';
+
+const editTodo = (inputDiv) => {
+  const input = inputDiv;
+  input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const description = input.value;
+      const inputId = input.id;
+      if (description !== '') {
+      editList(description,inputId);
+      window.location.reload();
+    } else {
+      document.querySelector('.emptylist').style.display = 'block';
+    }
+    }
+  });
+};
+export default editTodo;
